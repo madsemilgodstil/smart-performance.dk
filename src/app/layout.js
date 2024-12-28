f8205f5d9250edc0f15fa60fd9125c94ef168c71
madsemilgodstil <mads93n2@stud.kea.dk>
2024-12-28 20:02:42 +0100

    ddd

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,8 @@ import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import Footer from '@/components/footer/Footer'
 import ClarityProvider from '@/components/clarity/ClarityProvider'
+import Head from 'next/head'
+import Script from 'next/script' // Import Script from Next.js
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -21,9 +23,33 @@ export const metadata = {
 export default function RootLayout ({ children }) {
   return (
     <html lang='en' className='dark'>
+      <Head />
+      {/* Google Tag Manager Script */}
+      <Script
+        id='google-tag-manager'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WD3F7KX7');
+          `
+        }}
+      />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src='https://www.googletagmanager.com/ns.html?id=GTM-WD3F7KX7'
+            height='0'
+            width='0'
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <ClarityProvider />
         {children}
         <Footer />
