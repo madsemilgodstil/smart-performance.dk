39846c3ff8cd4335609bfe7420dd182e4fbd6f62
madsemilgodstil <mads93n2@stud.kea.dk>
2024-12-28 19:29:21 +0100

    clarity

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,10 @@
+'use client'
+
+import { useEffect } from 'react'
+import Clarity from 'clarity-js' // Import Clarity SDK
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import Footer from '@/components/footer/Footer'
-import Head from 'next/head'
-import Clarity from '@microsoft/clarity'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -20,22 +22,14 @@ export const metadata = {
 }
 
 export default function RootLayout ({ children }) {
+  useEffect(() => {
+    // Initialize Clarity with your project ID
+    const projectId = 'pl5x63xp77' // Replace "yourProjectId" with your actual Clarity project ID
+    Clarity.init(projectId)
+  }, [])
+
   return (
     <html lang='en' className='dark'>
-      <Head>
-        <script
-          type='text/javascript'
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "pl5x63xp77");
-            `
-          }}
-        />
-      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
