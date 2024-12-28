aedbef1754cc30d03ad356c0f82906a141caab98
madsemilgodstil <mads93n2@stud.kea.dk>
2024-12-28 18:58:46 +0100

    new new

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,18 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home () {
   return (
     <div className='flex justify-center items-center h-screen'>
-      <Image
-        src='/logo-white.png'
-        alt='Logo'
-        width={800}
-        height={800}
-        priority
-      />
+      <Link href='/'>
+        <Image
+          src='/logo-white.png'
+          alt='Logo'
+          width={800}
+          height={800}
+          priority
+        />
+      </Link>
     </div>
   )
 }
