e28d7fb7070c73d41740dcccee4977328bae3c9a
madsemilgodstil <mads93n2@stud.kea.dk>
2024-12-28 19:23:59 +0100

    header

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import Footer from '@/components/footer/Footer'
+import Head from 'next/head' // Import Head for custom <head> additions
+
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin']
@@ -19,6 +21,20 @@ export const metadata = {
 export default function RootLayout ({ children }) {
   return (
     <html lang='en' className='dark'>
+      <Head>
+        <script
+          type='text/javascript'
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "pl5x63xp77");
+            `
+          }}
+        />
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
